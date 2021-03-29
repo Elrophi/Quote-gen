eac5a168184b1e7b0b34bfe6a831483dc775bb6c
el <elrophilskwaila@gmail.com>
2021-03-30 02:06:36 +0300

    testing my quote and route

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -8,14 +8,10 @@ import { Quote } from '../quote';
 })
 export class QuotesComponent implements OnInit {
 
-  title = 'welcome to tech quotes';
+  title = 'welcome to Status-Quotes';
   quotes:Quote[] = [
-    new Quote (1,'Charles','Human spirit','The human spirit must prevail over technology to allow humans be in charge.','Albert Einstein', new Date(2019,7,12),0,0),
-    new Quote (2,'Wendy','Technology…','Technology… the knack of so arranging  that we don’t have to experience it.','Max Frisch',new Date(2019,7,14),0,0),
-    new Quote (3,'Mikey','The great myth','The great myth of our times is that technology is communication.','Libby Larsen',new Date(2019,6,3),0,0),
-    new Quote (4,'Beatrice','Communication','Communications tools don’t get socially interesting until they get technologically boring.','Clay Shirky',new Date(2019,7,7),0,0),
-    new Quote (5,'Charles','Code and people','Programs must be written for people to read, and only incidentally for machines to execute.','Harold Abelson',new Date(2019,7,17),0,0),
-    new Quote (6,'Alfred','Tech and life','For a list of all the ways technology has failed to improve the quality of life, please press three.','Alice Kahn',new Date(2019,7,15),0,0)
+    new Quote (1,'African','Life','he greatest glory in living lies not in never falling, but in rising every time we fall','Nelson Mandela', new Date(2021,1,12),0,0),
+    new Quote (2,'American','Humanity','Your time is limited, so do not waste it living someone elses life. Do not be trapped by dogma – which is living with the results of other peoples thinking.','Steve Jobs',new Date(2021,3,22),0,0),
   ];
   get sortQuotes() {
     return this.quotes.sort((a, b) => {
